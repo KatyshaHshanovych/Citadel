49fbc326cc210a0f5787a73d62dd303e193fed37
Andrey <a.cherewaty@gmail.com>
2021-04-04 19:48:40 +0300

    create a new repository

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mofy</title>
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="block-main">
+        <div class="header">
+            <div class="header__center">
+                <div class="header__logo">
+                    <a href="/">
+                        <img src="./img/logo.png" alt="logo" title="logo">
+                    </a>
+                </div>
+                <div class="header_navigation">
+                    <ul class="header__navMenu">
+                        <li>
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                About Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Our Service
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Contacts
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="header__iconContact">
+                    <a href="#">
+                        <img src="./img/email.png" alt="email" title="email">
+                    </a>
+                    <a href="#">
+                        <img src="./img/telephone.png" alt="tel" title="tel">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
