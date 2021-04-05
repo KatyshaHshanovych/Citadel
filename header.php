babeb293e92d2e566b457e83fb416f6d5ba32666
Andrey <a.cherewaty@gmail.com>
2021-04-05 09:31:06 +0300

    add gitignore for project

---
+++ header.php
@@ -0,0 +1,41 @@
+<div class="header">
+    <div class="header__center">
+        <div class="header__logo">
+            <a href="/">
+                <img src="./img/logo.png" alt="logo" title="logo">
+            </a>
+        </div>
+        <div class="header__navigation">
+            <ul class="header__navMenu">
+                <li class="header__navLink">
+                    <a href="#" class="header__links">
+                        Home
+                    </a>
+                </li>
+                <li class="header__navLink">
+                    <a href="#" class="header__links">
+                        About Us
+                    </a>
+                </li>
+                <li class="header__navLink">
+                    <a href="#" class="header__links">
+                        Our Service
+                    </a>
+                </li>
+                <li class="header__navLink">
+                    <a href="#" class="header__links">
+                        Contacts
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="header__iconContact">
+            <a href="#" class="header__iconTel">
+                <img src="./img/email.png" alt="email" title="email">
+            </a>
+            <a href="#">
+                <img src="./img/telephone.png" alt="tel" title="tel">
+            </a>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -5,50 +5,109 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mofy</title>
+    <title>Citadel</title>
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mixin.css">
 </head>
 <body>
     <div class="block-main">
-        <div class="header">
-            <div class="header__center">
-                <div class="header__logo">
-                    <a href="/">
+        <div class="block-center">
+        </div>
+        <?php include 'header.php'; ?>
+        Hello
+        Hello
+        Hello
+        <div class="footer">
+            <div class="block-center">
+                <div class="footer__first">
+                    <div class="footer__logo">
                         <img src="./img/logo.png" alt="logo" title="logo">
-                    </a>
+                    </div>
+                    <div class="footer__aboutCompany">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Who We Are
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    What We Do
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Our Services
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Privacy Policy
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer__headOffice">
+                        <div class="footer__mainInfo">
+                            <div class="footer__contactIcon">
+                                <img src="./img/icon/spot.png" alt="spot" title="spot">
+                            </div>
+                            <div class="footer__contactInfo">
+                                Lorclon House 139B The Broadway<br>
+                                West Ealing, London W13 9BE
+                            </div>
+                        </div>
+                        <div class="footer__mainInfo">
+                            <div class="footer__contactIcon">
+                                <img src="./img/icon/call.png" alt="call" title="call">
+                            </div>
+                            <div class="footer__contactInfo">
+                                020 8904 9272
+                            </div>
+                        </div>
+                        <div class="footer__mainInfo">
+                            <div class="footer__contactIcon">
+                                <img src="./img/icon/tel.png" alt="fax" title="fax">
+                            </div>
+                            <div class="footer__contactInfo">
+                                020 8904 9262
+                            </div>
+                        </div>
+                        <div class="footer__mainInfo">
+                            <div class="footer__contactIcon">
+                                <img src="./img/icon/message.png" alt="email" title="email">
+                            </div>
+                            <div class="footer__contactInfo">
+                                office@lorclon.com
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer__regionalOffice">
+                        <div class="footer__mainInfo">
+                            <div class="footer__contactIcon">
+                                <img src="./img/icon/spot.png" alt="spot" title="spot">
+                            </div>
+                            <div class="footer__contactInfo">
+                                Lorclon Yard, Arden Eco Park <br>
+                                Coventry Road, Bickenhall, Solihull<br>
+                                West Midlands B92 0DY
+                            </div>
+                        </div>
+                        <div class="footer__mainInfo">
+                            <div class="footer__contactIcon">
+                                <img src="./img/icon/call.png" alt="call" title="call">
+                            </div>
+                            <div class="footer__contactInfo">
+                                0121 661 7488
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="header_navigation">
-                    <ul class="header__navMenu">
-                        <li class="header__navLink">
-                            <a href="#" class="header__links">
-                                Home
-                            </a>
-                        </li>
-                        <li class="header__navLink">
-                            <a href="#" class="header__links">
-                                About Us
-                            </a>
-                        </li>
-                        <li class="header__navLink">
-                            <a href="#" class="header__links">
-                                Our Service
-                            </a>
-                        </li>
-                        <li class="header__navLink">
-                            <a href="#" class="header__links">
-                                Contacts
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="header__iconContact">
-                    <a href="#">
-                        <img src="./img/email.png" alt="email" title="email">
-                    </a>
-                    <a href="#">
-                        <img src="./img/telephone.png" alt="tel" title="tel">
-                    </a>
+                <div class="footer__second">
+                    <div class="footer__copyright">
+                        ‍© 2021 Copyright Lorclon Ltd All Rights Reserved
+                    </div>
                 </div>
             </div>
         </div>
